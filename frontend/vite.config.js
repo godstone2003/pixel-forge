753aef7b352dde64e8d661acf53298438251606e
Anand Mishra <ayush19november@gmail.com>
2025-08-05 00:53:35 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
 // https://vite.dev/config/
 export default defineConfig({
   base: '/',
@@ -10,4 +9,10 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  server: {
+    host: true, // Binds to 0.0.0.0 so it's accessible externally
+    port: 3000, // Or use 80 if you mapped that in Docker
+    allowedHosts: 'all', // OR use an array with your EC2 DNS
+    // allowedHosts: ['ec2-3-17-12-25.us-east-2.compute.amazonaws.com']
+  }
 })
